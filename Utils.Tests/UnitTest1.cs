6290eaa8e3bf390cfbca50aab0f40f0abbea0362
jakaz <wfz09317@163.com>
2018-12-27 17:21:47 +0800

    del

--- Utils.Tests/UnitTest1.cs
+++ Utils.Tests/UnitTest1.cs
@@ -1,9 +1,4 @@
-﻿using Dapper;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MySql.Data.MySqlClient;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Utils.Tests
 {
@@ -13,17 +8,6 @@ namespace Utils.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var conn = new MySqlConnection("Server=192.168.0.199;Port=3306;UserId=root;Password=newsys;Database=redcross");
-            conn.Open();
-            var reader = conn.ExecuteReader("SELECT * FROM `sys_filecontent`");
-            while (reader.Read())
-            {
-                var ms = new MemoryStream((byte[])reader.GetValue(1));
-                var bm = new Bitmap(ms);
-                bm.Save(@"D:/foo/" + reader.GetString(0) + ".png", ImageFormat.Png);
-            }
-            reader.Close();
-            conn.Close();
         }
     }
 }
